d4ee176f68475838058b667a8837999700b647d1
alaa eddine <alaago112000@gmail.com>
2024-03-20 14:53:28 +0100

    add feature or property local storige

--- indexC.js
+++ indexC.js
@@ -1,9 +1,16 @@
 let moon = document.getElementById('moon');
+if (localStorage.getItem('mode')!=null) {
+    document.body.classList.add('dark-theme')
+    moon.src='imagesC/sun.png'
+}
 moon.addEventListener('click',function(){
     document.body.classList.toggle('dark-theme')
     if (document.body.classList.contains('dark-theme')) {
         moon.src='imagesC/sun.png'
+        localStorage.setItem('mode','dark-them')
     }else{
         moon.src='imagesC/moon.png'
+        localStorage.clear('mode')
     }
+    console.log(document.body.classList)
 })
